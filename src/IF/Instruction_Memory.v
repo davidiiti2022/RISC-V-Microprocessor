d4e76d655c90ab691b89fec4745c7d4b29a8a2aa
davidiiti2022 <davidku556@gmail.com>
2026-03-29 12:37:33 +0530

    Add Instruction_Memory module for instruction fetching

---
+++ src/IF/Instruction_Memory.v
@@ -0,0 +1,9 @@
+module Instruction_Memory(input [31:0] addr, output [31:0] instr);
+reg [31:0] mem[0:255];
+
+initial begin
+    $readmemh("program.mem", mem);
+end
+
+assign instr = mem[addr[9:2]];
+endmodule
